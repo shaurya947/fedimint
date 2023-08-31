240d1dfacce22d8550283d8c24e77dd11e7f3507
Shaurya Arora <shaurya.arora1@gmail.com>
2023-08-30 23:09:05 -0400

    Add client/server modules to binaries

--- fedimint-cli/src/main.rs
+++ fedimint-cli/src/main.rs
@@ -1,7 +1,8 @@
 use fedimint_cli::FedimintCli;
+use stabilitypool_client::PoolClientGen;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    FedimintCli::new()?.with_default_modules().run().await;
+    FedimintCli::new()?.with_default_modules().with_module(PoolClientGen).run().await;
     Ok(())
 }
--- fedimintd/src/bin/main.rs
+++ fedimintd/src/bin/main.rs
@@ -1,6 +1,7 @@
 use fedimintd::fedimintd::Fedimintd;
+use stabilitypool_server::PoolConfigGenerator;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Fedimintd::new()?.with_default_modules().run().await
+    Fedimintd::new()?.with_default_modules().with_module(PoolConfigGenerator).run().await
 }
